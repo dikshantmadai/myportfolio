9bac359a97f287e7273ede43453811e430494a27
Dikshantmadai <122090500+Dikshantmadai@users.noreply.github.com>
2023-09-20 20:33:11 +0545

    7

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,58 +1,184 @@
 import React from 'react'
+import "./services.css";
 
 const Services = () => {
     return (
         <section className="services section" id="services">
             <h2 className="section__title">Services</h2>
-         <span className="section__subtitle">What i offer</span>
+            <span className="section__subtitle">What i offer</span>
 
-         <div className="services__container container grid">
-            <div className="services__content">
-                <div>
-                    <i className="uil uil services__icon"></i>
-                    <h3 className="services__title"></h3>
-                </div>
-
-                <span className="services__button">View More <i className="uil uil-arrow-right services__button-icon"></i></span>
-
-                <div className="services__modal">
-                    <div className="services__modal-content">
-                        <i className="uil uil-times services__modal-close"></i>
-
-                        <h3 className="services__modal-title"></h3>
-
-                        <p className="services__modal-description">
-                            Service with more than 2 years of experience.Providing quality work to clients and companies.
-                        </p>
-
-                        <ui className="services__modal-services grid">
-                            <li className="services__modal-service">
-                                <i className="uil uil-check-circle services__modal-icon"></i>
-                                <p className="services__modal-info"> I collect, clean, and analyze data to uncover insights that inform business decisions and strategies</p>
-                            </li>
-
-                            <li className="services__modal-service">
-                                <i className="uil uil-check-circle services__modal-icon"></i>
-                                <p className="services__modal-info"> Data Visualization: Create clear and compelling visualizations.</p>
-                            </li>
-
-                            <li className="services__modal-service">
-                                <i className="uil uil-check-circle services__modal-icon"></i>
-                                <p className="services__modal-info"> Predictive Analysis: Anticipate future trends and outcomes.</p>
-                            </li>
-
-                            
-                        </ui>
-
+            <div className="services__container container grid">
+                <div className="services__content">
+                    <div>
+                        <i className="uil uil-edit services__icon"></i>
+                        <h3 className="services__title">Data <br /> analysis</h3>
                     </div>
+
+                    <span className="services__button">View More <i className="uil uil-arrow-right services__button-icon"></i></span>
+
+                    <div className="services__modal">
+                        <div className="services__modal-content">
+                            <i className="uil uil-times services__modal-close"></i>
+
+                            <h3 className="services__modal-title">Data analysis </h3>
+
+                            <p className="services__modal-description">
+                                Service with more than 2 years of experience.Providing quality work to clients and companies.
+                            </p>
+
+                            <ui className="services__modal-services grid">
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info"> I collect, clean, and analyze data to uncover insights that inform business decisions and strategies</p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info"> Exploratory Data Analysis (EDA).</p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info"> Data Modeling & Analysis</p>
+                                </li>
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info"> Data Visualization</p>
+                                </li>
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info"> Data Strategy & Consulting</p>
+                                </li>
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">Data Reporting & Communication</p>
+                                </li>
+
+
+                            </ui>
+
+                        </div>
+                    </div>
+
                 </div>
+
+                <div className="services__content">
+                    <div>
+                        <i className="uil uil-web-grid services__icon"></i>
+                        <h3 className="services__title">Frontend <br /> Developer</h3>
+                    </div>
+
+                    <span className="services__button">View More <i className="uil uil-arrow-right services__button-icon"></i></span>
+
+                    <div className="services__modal">
+                        <div className="services__modal-content">
+                            <i className="uil uil-times services__modal-close"></i>
+
+                            <h3 className="services__modal-title">Frontend Developer</h3>
+
+                            <p className="services__modal-description">
+                                Service with more than 2 years of experience.Providing quality work to clients and companies.
+                            </p>
+
+                            <ui className="services__modal-services grid">
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info"> I collect, clean, and analyze data to uncover insights that inform business decisions and strategies</p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info"> Exploratory Data Analysis (EDA).</p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info"> Data Modeling & Analysis</p>
+                                </li>
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info"> Data Visualization</p>
+                                </li>
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info"> Data Strategy & Consulting</p>
+                                </li>
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">Data Reporting & Communication</p>
+                                </li>
+
+
+                            </ui>
+
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="services__content">
+                    <div>
+                        <i className="uil uil-arrow services__icon"></i>
+                        <h3 className="services__title">Ui/Ux <br /> Designer</h3>
+                    </div>
+
+                    <span className="services__button">View More <i className="uil uil-arrow-right services__button-icon"></i></span>
+
+                    <div className="services__modal">
+                        <div className="services__modal-content">
+                            <i className="uil uil-times services__modal-close"></i>
+
+                            <h3 className="services__modal-title">Ui/Ux Designer</h3>
+
+                            <p className="services__modal-description">
+                                Service with more than 2 years of experience.Providing quality work to clients and companies.
+                            </p>
+
+                            <ui className="services__modal-services grid">
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info"> I collect, clean, and analyze data to uncover insights that inform business decisions and strategies</p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info"> Exploratory Data Analysis (EDA).</p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info"> Data Modeling & Analysis</p>
+                                </li>
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info"> Data Visualization</p>
+                                </li>
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info"> Data Strategy & Consulting</p>
+                                </li>
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">Data Reporting & Communication</p>
+                                </li>
+
+
+                            </ui>
+
+                        </div>
+                    </div>
+
+                </div>
+
 
             </div>
-         </div>
+
+            
 
         </section>
 
-    )
-}
+    );
+};
 
 export default Services
+// 1:32
